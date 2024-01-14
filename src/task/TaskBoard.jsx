773fa5802e30cc08b0ed82c1d058abcc193c7f5a
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-14 18:31:16 +0530

    add new task complete

--- src/task/TaskBoard.jsx
+++ src/task/TaskBoard.jsx
@@ -1,4 +1,5 @@
 
+import AddTaskModal from "./AddTaskModal";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
@@ -18,16 +19,23 @@ export default function TaskBoard() {
   }
 
 const [tasks,setTasks] = useState([defaultTask])
+const [showAddModal, setShowAddModal] = useState(false);
 
+function handleAddTask(newTask){
+  console.log('object',newTask);
+  setTasks([...tasks,newTask])
+  setShowAddModal(false); 
+}
 
   return (
     <section className="mb-20" id="tasks">
+    {showAddModal && <AddTaskModal onSave={handleAddTask}/>}
       <div className="container mx-auto">
         <div className="p-2 flex justify-end">
           <SearchTask />
         </div>
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
+          <TaskActions onAddClick={()=>setShowAddModal(true)}/>
           <TaskList tasks={tasks} />
         </div>
       </div>
