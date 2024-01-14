1b281f8b5f20f2181e075cb73b426b3f23724905
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-14 16:59:09 +0530

    header,hero,footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import "./App.css"
+import Footer from "./Footer";
+import Header from "./Header";
+import Hero from "./Hero";
+
 export default function App() {
-  return <div>App</div>;
+  return <div>
+    <Header/>
+    <Hero/>
+    <Footer/>
+  </div>;
 }
