cc3d3bdf8db782941a1be429a2ce25c52ce7a845
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-01-14 17:55:38 +0530

    TaskBoard static functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css"
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
+import TaskBoard from "./task/TaskBoard";
 
 export default function App() {
   return <div>
     <Header/>
     <Hero/>
+    <TaskBoard/>
     <Footer/>
   </div>;
 }
